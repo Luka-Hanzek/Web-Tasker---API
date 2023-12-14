53d04c71d7aa60116e4bf7472d89bfd8b0c9a0c9
Luka Hanzek <luka.hanzek2@gmail.com>
2023-12-14 17:06:16 +0100

    Improve pydantic models

--- src/rest/schemas/task.py
+++ src/rest/schemas/task.py
@@ -8,13 +8,23 @@ class _TaskIdentity(BaseModel):
     id: int
 
 
-class _TaskInfo(BaseModel):
+class _TaskProject(BaseModel):
     project_id: int
+
+
+class _TaskInfo(BaseModel):
     description: str
+
+
+class _TaskOwner(BaseModel):
     owner_username: str
 
 
-class _TaskData(BaseModel):
+class _TaskOwner(BaseModel):
+    owner_username: str
+
+
+class _TaskTime(BaseModel):
     start_timestamp: Optional[datetime.datetime]
     end_timestamp: Optional[datetime.date]
 
@@ -23,6 +33,6 @@ class TaskCreate(_TaskInfo):
     pass
 
 
-class TaskGet(_TaskIdentity, _TaskInfo, _TaskData):
+class TaskGet(_TaskIdentity, _TaskProject, _TaskInfo, _TaskOwner, _TaskTime):
     class Config:
         orm_mode = True
